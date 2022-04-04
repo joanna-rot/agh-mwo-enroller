9b47f82e37c01a3ef078e45dafbdbdcb9744d978
Jacek Dajda <dajda@agh.edu.pl>
2022-04-04 02:15:26 +0200

    Fixes for SQLite identity problem

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -22,4 +23,24 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	public Participant findByLogin(String login) {
+		//String hql = "FROM Participant WHERE login= ;
+		//Query query = connector.getSession().createQuery(hql);
+		return (Participant) connector.getSession().get(Participant.class, login);
+
+	}
+
+	public Participant add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.hashCode();
+		return participant;
+	}
+
+	public Participant delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.hashCode();
+		return participant;
+	}
 }
